b5f7c4e1f29741fe9688de8cdd1a9dc4ae0c8efc
Deepak Kumar <deepak.groobizz@gmail.com>
2024-03-21 22:37:49 +0530

    added random gif components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Tag from "./components/Tag";
+import Random from "./components/Random"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="relative flex flex-col items-center w-full h-screen overflow-x-hidden background">
+      <h1 className=" bg-white rounded-lg w-11/12 text-center mt-[40px]
+       px-10 py-2 text-4xl font-bold ">
+        RANDOM GIFS</h1>
+      <div className="flex flex-col w-full items-center gap-y-10 mt-[30px]">
+        <Random />
+        <Tag />
+      </div>
     </div>
   );
 }
